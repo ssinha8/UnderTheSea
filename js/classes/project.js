a7a09afb022df4b2b5939cdd8310de1756f7a3cb
Shloak Sinha <ssinha8@ucsc.edu>
2024-05-30 14:50:41 -0700

    init

---
+++ js/classes/project.js
@@ -0,0 +1,22 @@
+// class.js - purpose and description here
+// Author: Your Name
+// Date:
+
+/**
+ * Description of function
+ * @function
+ * @param {vartype} - what its purpose is
+ */
+class className {
+  // constructor function
+  constructor(param1, param2) {
+    // set properties using 'this' keyword
+    this.property1 = param1;
+    this.property2 = param2;
+  }
+
+  // define a method
+  myMethod() {
+    // code to run when method is called
+  }
+}
---
+++ js/sketch.js
@@ -0,0 +1,11 @@
+// sketch.js - purpose and description here
+// Author: Your Name
+// Date:
+
+// Globals
+
+// setup() function is called once when the program starts
+function setup() {}
+
+// draw() function is called repeatedly, it's the main animation loop
+function draw() {}
